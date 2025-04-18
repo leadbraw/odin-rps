5138210f3b021a3ba722769ce8d235012c8cef4a
Noah <noah.dangerfield@gmail.com>
2025-04-17 22:32:24 -0700

    getComputerChoice made

--- rps.js
+++ rps.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    const choice = Math.floor(Math.random() * (2 - 0) + 0);
+    let result;
+    switch (choice) {
+        case 0:
+            result = 'rock';
+            break;
+        case 1:
+            result = 'paper';
+            break;
+        case 2:
+            result = 'scissors';
+            break;
+    }
+    return result;
+}
