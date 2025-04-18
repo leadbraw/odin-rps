a177329e1da5d605d7febd8552af1c24bd198cec
Noah <noah.dangerfield@gmail.com>
2025-04-17 22:46:16 -0700

    playRound logic implemented

--- rps.js
+++ rps.js
@@ -35,4 +35,24 @@ function getHumanChoice() {
     return result;
 }
 
-console.log(getHumanChoice())
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`It's a tie! You both picked ${humanChoice}.`);
+    } else if (
+        humanChoice === 'rock' && computerChoice === 'scissors' ||
+        humanChoice === 'scissors' && computerChoice === 'paper' ||
+        humanChoice === 'paper' && computerChoice === 'rock'
+    ) {
+        console.log(`You win! You chose ${humanChoice} and the computer chose ${computerChoice}.`)
+        humanScore++;
+    } else {
+        console.log(`You lose :( You chose ${humanChoice} and the computer chose ${computerChoice}.`)
+        computerScore++;
+    }
+}
+
+let humanScore = 0, computerScore = 0;
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
