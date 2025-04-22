f9c97bb4e9800740295bfcc507f8023073cb2ddf
Noah <noah.dangerfield@gmail.com>
2025-04-21 22:55:46 -0700

    adding event listeners

--- rps.js
+++ rps.js
@@ -59,14 +59,27 @@ let scissorsButton = document.createElement('button');
 let textDiv = document.createElement('div');
 let buttonDiv = document.createElement('div');
 
+// Add event listeners. Probably a more efficient way to do this, but it doesn't matter here:
+rockButton.addEventListener("click", () => {
+    playRound('rock', getComputerChoice());
+});
+rockButton.addEventListener("click", () => {
+    playRound('paper', getComputerChoice());
+});
+rockButton.addEventListener("click", () => {
+    playRound('scissors', getComputerChoice());
+});
+
 rockButton.style.cssText = "border-radius: 25px; background:rgb(137, 180, 250); padding: 20px; width: 200px; height: 150px; font-family: sans-serif; font-size: 40px;";
 paperButton.style.cssText = "border-radius: 25px; background:rgb(137, 180, 250); padding: 20px; width: 200px; height: 150px; font-family: sans-serif; font-size: 40px;";
 scissorsButton.style.cssText = "border-radius: 25px; background:rgb(137, 180, 250); padding: 20px; width: 200px; height: 150px; font-family: sans-serif; font-size: 40px;";
-mainDiv.style.cssText = "border-radius: 20px; border: 2px solid rgb(137, 180, 250); background: rgb(24, 24, 37); display: flex; flex-direction: column; justify-content: center; padding: 20px; font-size: 40px; font-family: sans-serif; font-weight: bold; color: rgb(137, 180, 250);";
-textDiv.style.cssText = "display: flex; justify-content: center; align-content: center; margin-bottom: 20px;"
-buttonDiv.style.cssText = "display: flex; justify-content: space-between;"
-textDiv.textContent = "Rock Paper Scissors";
 
+mainDiv.style.cssText = `border-radius: 20px; border: 2px solid rgb(137, 180, 250); background: rgb(24, 24, 37); display: flex; flex-direction: column;
+                        justify-content: center; padding: 20px; font-size: 40px; font-family: sans-serif; font-weight: bold; color: rgb(137, 180, 250);`;
+textDiv.style.cssText = "display: flex; justify-content: center; align-content: center; margin-bottom: 20px;";
+buttonDiv.style.cssText = "display: flex; justify-content: space-between;";
+
+textDiv.textContent = "Rock Paper Scissors";
 rockButton.textContent = "Rock";
 paperButton.textContent = "Paper";
 scissorsButton.textContent = "Scissors";
